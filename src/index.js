b90336399e6b5b4b4c357899fbd4590befe968c2
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2021-10-30 19:39:56 +0300

    add router, link

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      {/* <Route exact path='/' component={App}></Route>
+      <Route path='/Header' component={Header}></Route> */}
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+//rcfe
+import React from 'react';
+
+function Cart() {
+  return (
+    <div>
+      <h1>Корзина</h1>
+    </div>
+  );
+}
+
+export default Cart;
